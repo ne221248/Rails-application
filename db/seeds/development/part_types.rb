f9129497ab4cc24ff48a6470390c5cdfe89ca341
asadasouta <ne221248@senshu-u.jp>
2024-01-19 04:28:19 +0900

    シードデータ更新

--- db/seeds/development/part_types.rb
+++ db/seeds/development/part_types.rb
@@ -1,8 +1,7 @@
-types = ["CPU", "OS", "GPU", "ケース", "メモリ", "ストレージ", "マザーボード", "冷却機", "電源", "光学ドライブ"]
+types = ["CPU","GPU","OS","マザーボード","メモリ", "ストレージ", "電源","ケース", "冷却機",  "光学ドライブ"]
 
 0.upto(9) do |idx|
     PartType.create(
         kind: types[idx]
     )
 end
-        
